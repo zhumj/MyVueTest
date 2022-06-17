c8451e02d793b352ecc9c7611e3ca56a1e705c55
zhumj <z421789838@gmail.com>
2022-06-17 20:11:54 +0800

    updata  状态管理与持久化: Pinia 的使用.md

--- readme/状态管理与持久化：Pinia 的使用.md
+++ readme/状态管理与持久化：Pinia 的使用.md
@@ -78,6 +78,87 @@ export default class AppView extends Vue {
 </script>
 ```
 
+## 订阅/监听
+
+### $subscribe 对state的订阅监听
+```
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { userStore } from "./store";
+
+export default class AppView extends Vue {
+  mUserStore = userStore();
+
+  created() {
+    this.mUserStore.$subscribe(
+      (mutation, state) => {
+        /*
+         * mutation主要包含三个属性值：
+         *   events：当前state改变的具体数据，包括改变前的值和改变后的值等等数据
+         *   storeId：是当前store的id
+         *   type：用于记录这次数据变化是通过什么途径，主要有三个分别是
+         *         “direct” ：通过 action 变化的
+                   ”patch object“ ：通过 $patch 传递对象的方式改变的
+                   “patch function” ：通过 $patch 传递函数的方式改变的
+         */
+        // 我们就可以在此处监听store中值的变化，当变化为某个值的时候，去做一些业务操作之类的
+      },
+      //detached:布尔值，默认是 false，正常情况下，当订阅所在的组件被卸载时，订阅将被停止删除，
+      { detached: false }
+    );
+  }
+}
+</script>
+```
+
+### $onAction 对actions的订阅监听
+```
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { userStore } from "./store";
+
+export default class AppView extends Vue {
+  mUserStore = userStore();
+
+  created() {
+    this.mUserStore.$onAction(
+      ({
+        name, // name of the action
+        store, // store instance, same as `someStore`
+        args, // array of parameters passed to the action
+        after, // hook after the action returns or resolves
+        onError, // hook if the action throws or rejects
+      }) => {
+        const startTime = Date.now();
+        // 这将在执行“store”的操作之前触发
+        console.log(`Start "${name}" with params [${args.join(", ")}].`);
+
+        // 如果动作成功并在完全运行之后，这将触发。
+        after((result) => {
+          console.log(
+            `Finished "${name}" after ${
+              Date.now() - startTime
+            }ms.\nResult: ${result}.`
+          );
+        });
+
+        // 如果动作抛出或返回拒绝的承诺，这将触发
+        onError((error) => {
+          console.warn(
+            `Failed "${name}" after ${
+              Date.now() - startTime
+            }ms.\nError: ${error}.`
+          );
+        });
+      },
+      // 默认是 false，正常情况下，当订阅所在的组件被卸载时，订阅将被停止删除
+      false
+    );
+  }
+}
+</script>
+```
+
 ## 持久化
 
 ### 安装 Pinia 持久化插件: [pinia-plugin-persistedstate](https://www.npmjs.com/package/pinia-plugin-persistedstate)
--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,7 @@
     <el-button v-on:click="changeStoreLang">{{
       mAppStore.$state.lang
     }}</el-button>
-    <el-button v-on:click="changeStoreName">{{
-      mUserStore.getName + " " + mUserStore.$state.age
-    }}</el-button>
+    <el-button v-on:click="changeStoreName">{{ mName + " " + mAge }}</el-button>
   </nav>
   <router-view />
 </template>
@@ -43,6 +41,65 @@ export default class AppView extends Vue {
     this.mName = name.value;
     this.mAge = age.value;
     this.mAppStore.setLang(this.$i18n.locale);
+
+    this.mAppStore.$onAction(
+      ({
+        name, // name of the action
+        store, // store instance, same as `someStore`
+        args, // array of parameters passed to the action
+        after, // hook after the action returns or resolves
+        onError, // hook if the action throws or rejects
+      }) => {
+        const startTime = Date.now();
+        // 这将在执行“store”的操作之前触发
+        console.log(`Start "${name}" with params [${args.join(", ")}].`);
+
+        // 如果动作成功并在完全运行之后，这将触发。
+        after((result) => {
+          console.log(
+            `Finished "${name}" after ${
+              Date.now() - startTime
+            }ms.\nResult: ${result}.`
+          );
+        });
+
+        // 如果动作抛出或返回拒绝的承诺，这将触发
+        onError((error) => {
+          console.warn(
+            `Failed "${name}" after ${
+              Date.now() - startTime
+            }ms.\nError: ${error}.`
+          );
+        });
+      },
+      // 默认是 false，正常情况下，当订阅所在的组件被卸载时，订阅将被停止删除
+      false
+    );
+
+    this.mUserStore.$subscribe(
+      (mutation, state) => {
+        /*
+        * mutation主要包含三个属性值：
+        *   events：当前state改变的具体数据，包括改变前的值和改变后的值等等数据
+        *   storeId：是当前store的id
+        *   type：用于记录这次数据变化是通过什么途径，主要有三个分别是
+        *         “direct” ：通过 action 变化的
+                  ”patch object“ ：通过 $patch 传递对象的方式改变的
+                  “patch function” ：通过 $patch 传递函数的方式改变的
+        *
+        * */
+        // 我们就可以在此处监听store中值的变化，当变化为某个值的时候，去做一些业务操作之类的
+        console.log(mutation);
+        if (this.mName != state.name) {
+          this.mName = state.name;
+        }
+        if (this.mAge != state.age) {
+          this.mAge = state.age;
+        }
+      },
+      //detached:布尔值，默认是 false，正常情况下，当订阅所在的组件被卸载时，订阅将被停止删除，
+      { detached: false }
+    );
   }
 
   getElementPlusLang() {
