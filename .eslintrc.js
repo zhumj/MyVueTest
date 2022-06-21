0e90c44d971d549e6f89a04e29b00eff8cea0a50
zhumj <z421789838@gmail.com>
2022-06-21 14:12:00 +0800

    取消 LF 和 CTLF 的检验

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,14 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // 取消 LF 和 CTLF 的检验
+    "prettier/prettier": ["error", {
+      "endOfLine":"auto"
+    }],
+    // 'linebreak-style': [
+    //   0,
+    //   'error',
+    //   'windows',
+    // ],
   },
 };
