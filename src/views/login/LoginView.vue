937255243d4075b5c89c1456ab651cceb0871930
zhumj <z421789838@gmail.com>
2022-07-06 13:48:48 +0800

    增加用 ul和li、el-row和el-col 来做多行多列列表实例

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -12,26 +12,65 @@
       <p>{{ selectedDate }}</p>
       <el-button v-on:click="goMain">Go Main</el-button>
     </div>
+    <p>用ul和li来做多行多列列表</p>
     <div>
       <ul
         v-for="data in dataList"
         :key="data"
-        style="list-style: none; margin-top: 20px"
+        style="list-style: none; margin-top: 20px; background-color: blueviolet"
       >
         <li style="width: 25%; float: left">
           <!-- <el-card :body-style="{ padding: '10px' }" class="Main"></el-card> -->
           <el-card :body-style="{ padding: '0px' }" class="Main">
             <div class="Before">
-              <p style="background-color: burlywood">我是正面</p>
+              <p style="background-color: burlywood">我是正面{{ data }}</p>
               <i-twemoji-flag-china class="loginIcon" />
             </div>
             <div class="After">
-              <p style="background-color: burlywood">我是反面</p>
+              <p style="background-color: burlywood">我是反面{{ data }}</p>
               <i-twemoji-flag-china class="loginIcon" />
             </div>
           </el-card>
         </li>
       </ul>
+    </div>
+    <p>用el-row和el-col来做多行多列列表</p>
+    <div style="clear: both">
+      <el-row
+        v-for="index in dataList.length % 4 === 0
+          ? Math.floor(dataList.length / 4)
+          : Math.floor(dataList.length / 4) + 1"
+        :key="index"
+        :gutter="0"
+        style="width: auto"
+      >
+        <el-col
+          v-for="i in dataList.length <= 4
+            ? dataList.length
+            : index * 4 <= dataList.length
+            ? 4
+            : dataList.length % 4"
+          :key="i"
+          :span="6"
+        >
+          <div>
+            <el-card :body-style="{ padding: '0px' }" class="Main">
+              <div class="Before">
+                <p style="background-color: burlywood">
+                  我是正面{{ dataList[(index - 1) * 4 + i - 1] }}
+                </p>
+                <i-twemoji-flag-china class="loginIcon" />
+              </div>
+              <div class="After">
+                <p style="background-color: burlywood">
+                  我是反面{{ dataList[(index - 1) * 4 + i - 1] }}
+                </p>
+                <i-twemoji-flag-china class="loginIcon" />
+              </div>
+            </el-card>
+          </div>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
@@ -46,9 +85,11 @@ const selectedDate = ref("");
 const dataList = ref([] as number[]);
 
 onBeforeMount(() => {
+  dataList.value.length = 0;
   for (let i = 0; i < 8; i++) {
     dataList.value.push(i);
   }
+  console.log("数据条数 = ", dataList.value.length);
 });
 
 function goMain() {
