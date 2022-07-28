760730e64e2e2fe244be5bd5f12d7c0b25c8cd38
zhumj <z421789838@gmail.com>
2022-07-28 10:18:44 +0800

    公鸡打鸣:哦哦哦

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
+  publicPath: "./",
   configureWebpack: {
     plugins: [
       AutoImport({
